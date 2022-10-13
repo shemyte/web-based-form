6995f88e876cd76e74187a4e1c83c75194b56b6c
shemyte <shemaiahngala8@gmail.com>
2022-10-13 14:00:50 +0300

    Restrict registered user to log with same details

--- register.php
+++ register.php
@@ -7,12 +7,17 @@
         $password = $_POST["password"];
         $confirm_password = $_POST["confirm-password"];
         
-        $sql = "INSERT INTO collect_data(username,email,password,confirm_password) values('$username','$email','$password','$confirm_password')";
+        $check = "SELECT * FROM collect_data WHERE email = '$email'";
 
-        if(mysqli_query($conn, $sql)){
-            echo "record added successfully";
+        $result1 = mysqli_query($conn, $check);
+
+        $num1 = mysqli_num_rows($result1);
+        if($num1 == 1){
+            echo "Email already taken";
         }else{
-            echo "error";
+            $sql = "INSERT INTO collect_data(username,email,password,confirm_password) values('$username','$email','$password','$confirm_password')";
+            mysqli_query($conn, $sql);
+            echo "Registration Successfully";
         }
     }
 ?>
